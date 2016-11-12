776ffe0becd109827af9f9be7b177275525613d7
Ray <rayluo00@gmail.com>
2016-11-11 16:08:18 -0800

    Adding source files and Jsoup jar

---
+++ WordData.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Ray Luo on 7/30/2016.
+ */
+public class WordData {
+
+    private String word;
+    private int wordCount;
+
+    public WordData (String inputWord, int inputWordCount) {
+        this.word = inputWord;
+        this.wordCount = inputWordCount;
+    }
+
+    public String getWord () {
+        return this.word;
+    }
+
+    public int getWordCount () { return this.wordCount; }
+
+}
